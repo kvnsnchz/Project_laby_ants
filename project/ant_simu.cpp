1ac3ec71910ff5403eed5495be9197e0bf7c41db
kvnsnchz <kvn9616@gmail.com>
2020-01-10 05:17:44 +0100

    Adding OMP

--- project/ant_simu.cpp
+++ project/ant_simu.cpp
@@ -30,6 +30,7 @@ void advance_time( const labyrinthe& land, pheromone& phen,
                    std::vector<ant>& ants, std::size_t& cpteur )
 {
     start[1] = std::chrono::system_clock::now();
+    #pragma omp parallel for schedule(dynamic, 64) reduction(+:cpteur)
     for ( size_t i = 0; i < ants.size(); ++i )
         ants[i].advance(phen, land, pos_food, pos_nest, cpteur);
     end[1] = std::chrono::system_clock::now();
@@ -103,9 +104,6 @@ int main(int nargs, char* argv[])
             MPI_Wait(&request, &status);
         }
         
-
-        //MPI_Ssend();
-
     }
 
     if(rank == 0){
