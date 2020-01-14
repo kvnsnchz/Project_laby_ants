b8ab4e1c3a332ddf67d1c05918727fc93b01861f
kvnsnchz <kvn9616@gmail.com>
2020-01-14 03:20:47 +0100

    Adding size ants

--- project/ant_simu.cpp
+++ project/ant_simu.cpp
@@ -71,12 +71,14 @@ int main(int nargs, char* argv[])
 
     int ranks_0_1[2] = {0, 1};
     int ranks_1_n[nbp - 1];
-    const int buffer_size = 1 + 2*nb_ants + laby.dimensions().first*laby.dimensions().second;
-
+    int nb_ants_proc = nb_ants / (nbp - 1);
+    int nb_ants_total = nb_ants_proc*(nbp - 1);
+    const int buffer_size = 1 + 2*nb_ants_total + laby.dimensions().first*laby.dimensions().second;
+   
     for(int i = 1; i < nbp; i++){
         ranks_1_n[i - 1] = i; 
     }
-    
+
     MPI_Comm_group(MPI_COMM_WORLD, &world_group);
     if(rank == 1 || rank == 0){
         MPI_Group_incl(world_group, 2, ranks_0_1, &group_0_1);
@@ -90,9 +92,7 @@ int main(int nargs, char* argv[])
     if(rank >= 1){
         const double eps = 0.75;  // Coefficient d'exploration
         size_t food_quantity = 0;
-        int nb_ants_proc = nb_ants / (nbp - 1);
         int new_rank;
-
         // Définition du coefficient d'exploration de toutes les fourmis.
         ant::set_exploration_coef(eps);
         // On va créer toutes les fourmis dans le nid :
@@ -118,8 +118,9 @@ int main(int nargs, char* argv[])
             MPI_Status status;
             
             if(rank == 1){
-                std::vector<double>(nb_ants*2).swap(ants_buffer_recv);
+                std::vector<double>(2*nb_ants_total).swap(ants_buffer_recv);
             }
+
             std::vector<double>(laby.dimensions().first * laby.dimensions().second).swap(pher_buffer_recv);
             MPI_Reduce(&food_quantity, &food_quantity_buffer, 1, MPI_INT, MPI_SUM, 0, comm_1_n);
             MPI_Gather(ants_buffer.data(), ants_buffer.size(), MPI_DOUBLE, ants_buffer_recv.data(), ants_buffer.size(), MPI_DOUBLE, 0, comm_1_n);
@@ -130,12 +131,8 @@ int main(int nargs, char* argv[])
                 std::vector<double> buffer;
                 buffer.emplace_back((double)food_quantity_buffer);
                 buffer.insert(buffer.end(), ants_buffer_recv.begin(), ants_buffer_recv.end());
-
-                for ( std::size_t i = 0; i < laby.dimensions( ).first; ++i )
-                    for ( std::size_t j = 0; j < laby.dimensions( ).second; ++j ) {
-                        buffer.emplace_back((double)phen( i, j ));
-                    }
-
+                buffer.insert(buffer.end(), pher_buffer_recv.begin(), pher_buffer_recv.end());
+                
                 MPI_Isend(buffer.data(), buffer.size(), MPI_DOUBLE, 0, 101, comm_0_1, &request);        
                 advance_time(laby, phen, pos_nest, pos_food, ants, food_quantity);
                 MPI_Wait(&request, &status);
@@ -150,22 +147,19 @@ int main(int nargs, char* argv[])
 
     if(rank == 0){
         start[0] = std::chrono::system_clock::now();
-        int nb_ants_proc = nb_ants / (nbp - 1);
         std::vector<ant> ants;
-        ants.reserve(nb_ants);
+        std::vector<double> buffer_rec(buffer_size);
+        ants.reserve(nb_ants_total);
         size_t food_quantity = 0;
         size_t ants_start = 1;
-        size_t pher_start = nb_ants*2 + 1;
-
+        size_t pher_start = nb_ants_total*2 + 1;
         pheromone phen(laby.dimensions(), pos_food, pos_nest, alpha, beta);
         
         MPI_Status status;
-        std::vector<double> buffer_rec(buffer_size);
         
         MPI_Recv(buffer_rec.data(), buffer_rec.size(), MPI_DOUBLE, 1, 101, comm_0_1, &status);
 
-        food_quantity = buffer_rec[0];
-        
+        food_quantity = buffer_rec[0]; 
         for(size_t i = ants_start; i < pher_start; i += 2){
             ants.emplace_back(position_t(buffer_rec[i], buffer_rec[i+1]), life);
         }
@@ -173,16 +167,13 @@ int main(int nargs, char* argv[])
 
         gui::context graphic_context(nargs, argv);
         gui::window& win =  graphic_context.new_window(h_scal*laby.dimensions().second,h_scal*laby.dimensions().first+266);
-    
         display_t displayer( laby, phen, pos_nest, pos_food, ants, win );
-        
+
         gui::event_manager manager;
         manager.on_key_event(int('q'), [] (int code) { MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS);});
         manager.on_key_event(int('t'), [] (int code) { print_time_execution(); });
         manager.on_display([&] { displayer.display(food_quantity); win.blit(); });
         manager.on_idle([&] () { 
-            //advance_time(laby, phen, pos_nest, pos_food, ants, food_quantity);
-
             start[4] = std::chrono::system_clock::now();
             displayer.display(food_quantity); 
             end[4] = std::chrono::system_clock::now();
